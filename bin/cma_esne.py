b806428984b81240e4a1a4a0196c0fd243bac9f5
Brenda-Machado <87547436+Brenda-Machado@users.noreply.github.com>
2023-02-01 18:32:40 -0300

    adicionando pos avaliação ao cma_esne

--- bin/cma_esne.py
+++ bin/cma_esne.py
@@ -124,11 +124,43 @@ class Algo(EvoAlgo):
             seed= self.niches[self.niche]
         )
         self.steps += eval_length
-        if eval_rews> self.bestestfit:
-            self.bestestfit = eval_rews
+        if eval_rews > self.bestestfit[0]:
+            self.bestestfit = (eval_rews, candidate)
             print(eval_rews)
         self.niche += 1
-        return 1000 - eval_rews
+        self.fitness_eval.append(eval_rews)
+        return (1000 - eval_rews)
+
+    def pos_evaluate(self):
+        self.avgfit = np.average(self.fitness_eval)  # compute the average fitness
+
+        self.updateBest(
+            self.bestestfit
+        )  # Stored if it is the best obtained so far
+
+        # postevaluate best sample of the last generation
+        # in openaiesp.py this is done the next generation, move this section before the section "evaluate samples" to produce identical results
+        gfit = 0
+        if self.bestsol is not None:
+            self.policy.set_trainable_flat(self.bestsol)
+            self.tnormepisodes += self.inormepisodes
+            for t in range(self.policy.nttrials):
+                if (
+                    self.policy.normalize == 1
+                    and self.normepisodes < self.tnormepisodes
+                ):
+                    self.policy.nn.normphase(1)
+                    self.normepisodes += 1  # we collect normalization data
+                    self.normalizationdatacollected = True
+                else:
+                    self.policy.nn.normphase(0)
+                eval_rews, eval_length = self.policy.rollout(
+                    1, seed=(self.seed + 100000 + t)
+                )
+                gfit += eval_rews
+                self.steps += eval_length
+            gfit /= self.policy.nttrials
+            self.updateBestg(gfit, self.bestsol)
 
     def run(self):
 
@@ -147,7 +179,8 @@ class Algo(EvoAlgo):
                 self.nparams,
             )
         )
-        self.bestestfit = float('-inf')
+        self.bestestfit = (-99999999, None)
+        self.fitness_eval = []
 
         random_niches = []
         num_random_niches = self.number_niches*100
@@ -166,7 +199,7 @@ class Algo(EvoAlgo):
 
             self.cma_es.optimize(self.evaluate)
             self.result_pretty()
-            
+            self.pos_evaluate()
 
             self.stat = np.append(
                 self.stat,
