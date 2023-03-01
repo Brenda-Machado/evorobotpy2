abc776a5511633bfd615757d0eeb14ec7b9e0403
Brenda-Machado <araucaria2011@gmail.com>
2023-03-01 18:40:49 -0300

    nes and evograd

--- bin/nes.py
+++ bin/nes.py
@@ -19,6 +19,9 @@ from utils import ascendent_sort
 import sys
 import os
 import configparser
+import torch
+from evograd import expectation
+from evograd.distributions import Normal
 
 # Natural Evolution Strategies Algorithm
 
@@ -106,6 +109,7 @@ class Algo(EvoAlgo):
         self.m = zeros(self.nparams)  # Adam: momentum vector
         self.v = zeros(self.nparams)  # Adam: second momentum vector (adam)
         self.epsilon = 1e-08  # Adam: To avoid numerical issues with division by zero...
+        self.alpha = 0.2 # NES: learning rate
         self.beta1 = 0.9  # Adam: beta1
         self.beta2 = 0.999  # Adam: beta2
         self.bestgfit = -99999999  # the best generalization fitness
@@ -130,7 +134,7 @@ class Algo(EvoAlgo):
         fname = self.filedir + "/S" + str(self.seed) + ".fit"
         fp = open(fname, "w")  # save summary
         fp.write(
-            "Seed %d (%.1f%%) gen %d msteps %d bestfit %.2f bestgfit %.2f bestsam %.2f avgfit %.2f paramsize %.2f \n"
+            "Seed %d (%.1f%%) gen %d msteps %d bestfit %.2f bestgfit %.2f bestsam %.2f avgfit %.2f\n"
             % (
                 self.seed,
                 self.steps / float(self.maxsteps) * 100,
@@ -140,58 +144,47 @@ class Algo(EvoAlgo):
                 self.bestgfit,
                 self.bfit,
                 self.avgfit,
-                self.avecenter,
             )
         )
         fp.close()
 
-    # the difference between the ES algorithm and the NES is
-    # that NES follows the natural gradient, while ES follows the stochastic gradient
+    def evaluate(self, candidate):
+        
+        self.policy.set_trainable_flat(candidate)
+        self.policy.nn.normphase(
+            0
+        )  # normalization data is collected during the post-evaluation of the best sample of he previous generation
+        eval_rews, eval_length = self.policy.rollout(
+            self.policy.ntrials,
+            seed= self.seed
+        )
+        self.steps += eval_length
+        if eval_rews > self.bestestfit[0]:
+            self.bestestfit = (eval_rews, candidate)
 
-    def evaluate(self):
-        cseed = (
-            self.seed + self.cgen * self.batchSize
-        )  # Set the seed for current generation (master and workers have the same seed)
-        self.rs = np.random.RandomState(cseed)
-        self.samples = self.rs.randn(self.batchSize, self.nparams)
-        self.cgen += 1
+        self.fitness_eval.append(eval_rews)
 
-        # evaluate samples
-        candidate = np.arange(self.nparams, dtype=np.float64)
-        for b in range(self.batchSize):
-            for bb in range(2):
-                if bb == 0:
-                    candidate = self.center + self.samples[b, :] * self.noiseStdDev
-                else:
-                    candidate = self.center - self.samples[b, :] * self.noiseStdDev
-                self.policy.set_trainable_flat(candidate)
-                self.policy.nn.normphase(
-                    0
-                )  # normalization data is collected during the post-evaluation of the best sample of he previous generation
-                eval_rews, eval_length = self.policy.rollout(
-                    self.policy.ntrials,
-                    seed=(self.seed + (self.cgen * self.batchSize) + b),
-                )
-                self.samplefitness[b * 2 + bb] = eval_rews
-                self.steps += eval_length
-
-        fitness, self.index = ascendent_sort(self.samplefitness)  # sort the fitness
-        self.avgfit = np.average(fitness)  # compute the average fitness
-
-        self.bfit = fitness[(self.batchSize * 2) - 1]
-        bidx = self.index[(self.batchSize * 2) - 1]
-        if (bidx % 2) == 0:  # regenerate the genotype of the best samples
-            bestid = int(bidx / 2)
-            self.bestsol = self.center + self.samples[bestid] * self.noiseStdDev
-        else:
-            bestid = int(bidx / 2)
-            self.bestsol = self.center - self.samples[bestid] * self.noiseStdDev
-
-        self.updateBest(
-            self.bfit, self.bestsol
-        )  # Stored if it is the best obtained so far
+        self.stat = np.append(
+                self.stat,
+                [
+                    self.steps,
+                    self.bestfit,
+                    self.bestgfit,
+                    self.bfit,
+                    self.avgfit,
+                ],
+            )  # store performance across generations
+        
+        self.updateBest(self.bestestfit[0], self.bestestfit[1])
+            
+        return (eval_rews)    
+    
+    def pos_evaluate(self):
+        
+        self.avgfit = np.average(self.fitness_eval)  # compute the average fitness
 
         # postevaluate best sample of the last generation
+        # in openaiesp.py this is done the next generation, move this section before the section "evaluate samples" to produce identical results
         gfit = 0
         if self.bestsol is not None:
             self.policy.set_trainable_flat(self.bestsol)
@@ -214,60 +207,6 @@ class Algo(EvoAlgo):
             gfit /= self.policy.nttrials
             self.updateBestg(gfit, self.bestsol)
 
-    def optimize(self):
-
-        popsize = self.batchSize * 2  # compute a vector of utilities [-0.5,0.5]
-        utilities = zeros(popsize)
-        for i in range(popsize):
-            utilities[self.index[i]] = i
-        utilities /= popsize - 1
-        utilities -= 0.5
-
-        weights = zeros(
-            self.batchSize
-        )  # Assign the weights (utility) to samples on the basis of their fitness rank
-        for i in range(self.batchSize):
-            idx = 2 * i
-            weights[i] = (
-                utilities[idx] - utilities[idx + 1]
-            )  # merge the utility of symmetric samples
-
-        g = 0.0
-        i = 0
-        while (
-            i < self.batchSize
-        ):  # Compute the gradient (the dot product of the samples for their utilities)
-            gsize = -1
-            if (
-                self.batchSize - i < 500
-            ):  # if the popsize is larger than 500, compute the gradient for multiple sub-populations
-                gsize = self.batchSize - i
-            else:
-                gsize = 500
-            g += dot(weights[i : i + gsize], self.samples[i : i + gsize, :])
-            i += gsize
-        g /= popsize  # normalize the gradient for the popsize
-
-        if self.wdecay == 1:
-            globalg = -g + 0.005 * self.center  # apply weight decay
-        else:
-            globalg = -g
-
-        # adam stochastic optimizer
-        a = (
-            self.stepsize
-            * sqrt(1.0 - self.beta2 ** self.cgen)
-            / (1.0 - self.beta1 ** self.cgen)
-        )
-        self.m = self.beta1 * self.m + (1.0 - self.beta1) * globalg
-        self.v = self.beta2 * self.v + (1.0 - self.beta2) * (globalg * globalg)
-        dCenter = -a * self.m / (sqrt(self.v) + self.epsilon)
-
-        self.center += (
-            dCenter  # move the center in the direction of the momentum vectors
-        )
-        self.avecenter = np.average(np.absolute(self.center))
-
     def run(self):
 
         self.setProcess()  # initialize class variables
@@ -275,6 +214,8 @@ class Algo(EvoAlgo):
         last_save_time = start_time
         elapsed = 0
         self.steps = 0
+        self.bestestfit = (-99999999, None)
+        self.fitness_eval = []
         print(
             "NES: seed %d maxmsteps %d batchSize %d stepsize %lf noiseStdDev %lf wdecay %d symseed %d nparams %d"
             % (
@@ -291,10 +232,28 @@ class Algo(EvoAlgo):
 
         while self.steps < self.maxsteps:
 
-            self.evaluate()  # evaluate samples
+            self.fitnesses = []
 
-            self.optimize()  # estimate the gradient and move the centroid in the gradient direction
+            self.mu = torch.tensor(self.center, requires_grad=True)
 
+            p = Normal(self.mu, self.noiseStdDev)
+
+            pop = p.sample(n=self.batchSize)
+
+            for i in pop:
+                self.fitnesses.append(self.evaluate(i))
+
+            self.pos_evaluate()
+
+            self.fitnesses = torch.tensor(self.fitnesses , requires_grad=True)
+
+            mean = expectation(self.fitnesses, pop, p=p)
+
+            mean.backward()
+            with torch.no_grad():
+                self.mu += self.alpha * self.mu.grad
+                self.mu.grad.zero_()
+            
             self.stat = np.append(
                 self.stat,
                 [
@@ -303,7 +262,6 @@ class Algo(EvoAlgo):
                     self.bestgfit,
                     self.bfit,
                     self.avgfit,
-                    self.avecenter,
                 ],
             )  # store performance across generations
 
@@ -316,7 +274,7 @@ class Algo(EvoAlgo):
                 self.normalizationdatacollected = False
 
             print(
-                "Seed %d (%.1f%%) gen %d msteps %d bestfit %.2f bestgfit %.2f bestsam %.2f avg %.2f weightsize %.2f"
+                "Seed %d (%.1f%%) gen %d msteps %d bestfit %.2f bestgfit %.2f bestsam %.2f avg %.2f"
                 % (
                     self.seed,
                     self.steps / float(self.maxsteps) * 100,
@@ -326,7 +284,6 @@ class Algo(EvoAlgo):
                     self.bestgfit,
                     self.bfit,
                     self.avgfit,
-                    self.avecenter,
                 )
             )
 
