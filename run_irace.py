48f662d23614682201ed04e2bac137510e73f9eb
Arthur Holtrup Bianchini <alvaporta@outlook.com>
2023-03-31 13:29:11 -0300

    Update run_irace.py

--- run_irace.py
+++ run_irace.py
@@ -2,13 +2,12 @@ import subprocess
 
 def run_r_script():
     command = 'Rscript'
-    path2script = 'path/to/script.R'
+    path2script = '~/evorobotpy2/script.R'
     cmd = [command, path2script]
 
-    x = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = x.communicate()
+    with open('output.txt', 'w') as f:
+        x = subprocess.Popen(cmd, stdout=f, stderr=subprocess.STDOUT)
+        output, error = x.communicate()
 
     print('Output:', output)
     print('Error:', error)
-
-run_r_script()
